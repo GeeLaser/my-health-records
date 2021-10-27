5d99c030cccd0165c258ba2bc7800cc73ce67098
AllenLuGitHub <52037638+AllenLuGitHub@users.noreply.github.com>
2021-10-27 16:32:46 -0500

    re-added submit and recipient input and added app.post(/sendFile) in index.js

--- index.js
+++ index.js
@@ -172,11 +172,11 @@ app.get("/uploads/:file", (req, res) => {
 });
   
 
-app.get("/sendEmail/:file/:name", (req, res) => {
+app.get("/sendEmail/:file", (req, res) => {
 	
 	var message = {
 		from: userEmail,
-		to: "henryhmadsen@gmail.com",
+		to: "allenlu9326@gmail.com",
 		subject: "Test message",
 		text: "this is just a test email using nodemailer with an attachment",
 		attachments: [
@@ -198,6 +198,27 @@ app.get("/sendEmail/:file/:name", (req, res) => {
 
 	
 });
+
+app.post("/sendFile",function (req, res, next) {
+		
+	// Error MiddleWare for multer file upload, so if any
+	// error occurs, the image would not be uploaded!
+	upload(req,res,function(err) {
+
+		if(err) {
+			// ERROR occured (here it can be occured due
+			// to uploading image of size greater than
+			// 10MB or uploading different file type)
+			res.send(err);
+		}
+		else {
+			// SUCCESS, image successfully uploaded
+			res.status(200);
+			res.render('email');
+		}
+	})
+})
+
 // Take any port number of your choice which
 // is not taken by any other process
 app.listen(8080,function(error) {
