631b63a8c01a8dadd91634d03fde532275e71e35
Po4991212 <vinhphucvb@gmail.com>
2021-11-28 01:07:48 -0600

    adding download and upload feature

---
+++ build/precache-manifest.20bea50fbb26c09dba50921da662d824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc394d184b766d3a7b7e9f2be9e9f5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1aec92e729ac9006620",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a8fba09f90861ed2f68e",
+    "url": "/static/css/main.9a0772d8.chunk.css"
+  },
+  {
+    "revision": "e1aec92e729ac9006620",
+    "url": "/static/js/2.633074f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.633074f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fba09f90861ed2f68e",
+    "url": "/static/js/main.2e494a07.chunk.js"
+  },
+  {
+    "revision": "e1fcef772f1fdaabae77",
+    "url": "/static/js/runtime-main.466e3532.js"
+  }
+]);
---
+++ src/components/FilesList.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import download from 'downloadjs';
+import axios from 'axios';
+import { API_URL } from '../utils/constants';
+import Axios from 'axios';
+
+const FilesList = () => {
+  const [filesList, setFilesList] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    const getFilesList = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/getAllFiles`);
+        setErrorMsg('');
+        setFilesList(data);
+      } catch (error) {
+        error.response && setErrorMsg(error.response.data);
+      }
+    };
+
+    getFilesList();
+  }, []);
+
+  const deleteFile = async (id) =>  {
+    axios.delete(`${API_URL}/getAllFiles/delete/${id}`);
+  };
+
+  const downloadFile = async (id, path, mimetype) => {
+    try {
+      const result = await axios.get(`${API_URL}/download/${id}`, {
+        responseType: 'blob'
+      });
+      const split = path.split('/');
+      const filename = split[split.length - 1];
+      setErrorMsg('');
+      return download(result.data, filename, mimetype);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setErrorMsg('Error while downloading file. Try again later');
+      }
+    }
+  
+  };
+
+  return (
+    <div className="files-container">
+      {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+      <table className="files-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Download/Delete</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filesList.length > 0 ? (
+            filesList.map(
+              ({ _id, title, description, file_path, file_mimetype }) => (
+                <tr key={_id}>
+                  <td className="file-title">{title}</td>
+                  <td className="file-description">{description}</td>
+                  <td>
+                    <a
+                      href="#/"
+                      onClick={() =>
+                        downloadFile(_id, file_path, file_mimetype)
+                      }
+                    >
+                      Download/ 
+                    </a>
+                    <a
+                      href="#/"
+                      onClick={() =>
+                        deleteFile({_id,title})
+                      }
+                    >
+                      Delete 
+                    </a>
+                  </td>
+                  <td>
+                  <a
+                      href="#/"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              )
+            )
+          ) : (
+            <tr>
+              <td colSpan={3} style={{ fontWeight: '300' }}>
+                No files found. Please add some.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FilesList;
